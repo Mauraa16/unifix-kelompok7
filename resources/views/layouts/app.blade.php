a87d41c2e1aa31ef7ed5b17f892686d0a1a06a8f
Achmadghalib725 <achmad.ghalib725@gmail.com>
2025-12-06 18:46:57 +0700

    tambah icon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,6 +7,7 @@
 
     <title>{{ config('app.name', 'UNIFIX') }}</title>
 
+    <link rel="icon" type="image/png" href="{{ asset('images/icon.png') }}">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap" rel="stylesheet">
